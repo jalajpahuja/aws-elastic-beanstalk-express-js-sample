60734638ad2f7be89c88eb5a65f2587ec17c9d02
Jalaj Pahuja <jalaj.pahuja@hitachivantara.com>
2020-11-26 09:54:25 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('It's a test deployment pipeline handson'));
+app.get('/', (req, res) => res.send('It's a test deployment pipeline handson -- With Full Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
